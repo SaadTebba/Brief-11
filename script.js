4ada00f2f56b3b7825a294f9d38b2d0ad295b342
Saad Tebba <116269919+SaadTebba@users.noreply.github.com>
2023-01-03 08:23:37 +0100

    =

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// fetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-fetch("https://reqres.in/api/users")
-.then(res => res.json())
+let searchButton = document.getElementById('searchButton')
+
+let input = document.getElementById('searchbar').value.trim();
+
+fetch('www.themealdb.com/api/json/v1/1/filter.php?i=egg')
+.then(response => response.json())
 .then(data => console.log(data))
